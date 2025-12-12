23727fad64c1b8b6aeade41c940fc019bf0fc0d0
Itohan1 <momoduitohan5@gmail.com>
2025-12-12 12:27:23 +0100

    Updating user schema, chaning port

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,6 @@ connectDB();
 app.use("/api/v1", authRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
